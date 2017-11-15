edf71b3397f5ecd4269d05db6427eafeddd0c395
rannylais <rannylais20@hotmail.com>
2017-11-15 20:11:41 -0300

    prova

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from operator import attrgetter
 
 
 class Post(models.Model):
@@ -9,11 +10,18 @@ class Post(models.Model):
 	created_date = models.DateTimeField(
 		default=timezone.now)
 	published_date = models.DateTimeField(
-		blank=True, null=True)
-
+		blank=True, null=True, auto_now=True)
+	
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
 		return self.title
+
+
+class Estado(models.Model):
+	estado = models.CharField(max_length=50)
+	sigla = models.CharField(max_length=2)
+
+	
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -39,3 +39,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
